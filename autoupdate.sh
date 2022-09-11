eef44f5e1e133c84d21f4a22070bf7e5d30fbfde
412999826 <68527612+412999826@users.noreply.github.com>
2022-09-11 13:13:13 +0800

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
 get_latest_release() {
-	curl --silent "https://api.github.com/repos/$1/releases/latest" |	# Get latest release from GitHub api
-	grep '"tag_name":' |												# Get tag line
-	sed -E 's/.*"([^"]+)".*/\1/'										# Pluck JSON value
+	curl --silent "https://github.com/Dreamacro/clash/releases/tag/premium" |
+	grep '  <title>Release Premium ' |
+	sed -E 's/[^.^0-9]//g'
 }
 
-set -e $1
-
-latest_release_tag=`get_latest_release Dreamacro/clash`
-ver=`/usr/bin/clash -v`
+latest_release_tag=`get_latest_release`
 arch=`uname -m`
 [ $arch == 'x86_64' ] && board_id='amd64';
 [ $arch == 'aarch64' ] && board_id='armv8';
 [ $arch == 'armv7' ] && board_id='armv7';
 [ $arch == 'armv5' ] && board_id='armv5';
 
-if [[ $ver =~ $latest_release_tag ]]; then
-	echo 已是最新版本，升级中止！ && exit 1
-else
-	echo 准备更新到$latest_release_tag
-fi
 wget -q https://github.com/Dreamacro/clash/releases/download/$latest_release_tag/clash-linux-$board_id-$latest_release_tag.gz
 gzip -d clash-linux-$board_id-$latest_release_tag.gz
 chmod +x clash-linux-$board_id-$latest_release_tag
